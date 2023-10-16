94c8b572118e0dc74e7773b50fd10d7b081c825c
Basil Crow <me@basilcrow.com>
2023-10-16 15:11:07 -0700

    Add JDK 21

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 docker pull maven:3-eclipse-temurin-11
 docker pull maven:3-eclipse-temurin-17
+docker pull maven:3-eclipse-temurin-21
 pushd java-buildenv-11 2>/dev/null
 docker build -t java-buildenv-11 .
 popd 2>/dev/null
 pushd java-buildenv-17 2>/dev/null
 docker build -t java-buildenv-17 .
+popd 2>/dev/null
+pushd java-buildenv-21 2>/dev/null
+docker build -t java-buildenv-21 .
 popd 2>/dev/null
